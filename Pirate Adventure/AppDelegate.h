802cc346219345997d0c6c1eec47836ba29bb5a4
Karthik Kannan <apple@Apples-MacBook-Pro.local>
2014-11-28 21:34:01 +0530

    Initial Commit

---
+++ Pirate Adventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Karthik Kannan on 28/11/14.
+//  Copyright (c) 2014 Karthik Kannan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Adventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventure
+//
+//  Created by Karthik Kannan on 28/11/14.
+//  Copyright (c) 2014 Karthik Kannan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
